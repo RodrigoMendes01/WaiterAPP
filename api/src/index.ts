0440808a7ea49cc0d3a41ee479cb0386e94735f2
Rodrigo <rodrigo.rm123.rm@gmail.com>
2023-11-29 18:12:44 -0300

    feat: adding mongoDB models and stabilizing connection

---
+++ api/src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import { router } from './router';
+
+import 'dotenv/config';
+
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    const port = process.env.PORT;
+    const app = express();
+
+    app.use(express.json());
+
+    app.use(router);
+
+    app.listen(port, () => {
+      console.log(`🚀 Server is running at http://localhost:${port}`);
+    });
+  })
+  .catch((error) => console.log(error));
+
