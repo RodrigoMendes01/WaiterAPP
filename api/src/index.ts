7aad6a5603b0cd854bfb68f7fa7a8cb02e49e586
Rodrigo <rodrigo.rm123.rm@gmail.com>
2023-11-29 21:45:45 -0300

    chore: adding multer

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import path from 'node:path';
 import { router } from './router';
 
 import 'dotenv/config';
@@ -8,6 +9,8 @@ mongoose.connect('mongodb://localhost:27017')
   .then(() => {
     const port = process.env.PORT;
     const app = express();
+
+    app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
     app.use(express.json());
 
